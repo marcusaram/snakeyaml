12eca8127b2b3545c57e9ed09425b7a959ab5fb4
somov <somov@localhost>
2008-11-05 18:17:49 +0100

    add serialVersionUID to YAMLException

--- src/main/java/org/yaml/snakeyaml/error/YAMLException.java
+++ src/main/java/org/yaml/snakeyaml/error/YAMLException.java
@@ -7,6 +7,8 @@ package org.yaml.snakeyaml.error;
  * @see PyYAML 3.06 for more information
  */
 public class YAMLException extends RuntimeException {
+    private static final long serialVersionUID = -4738336175050337570L;
+
     public YAMLException(final String message) {
         super(message);
     }
