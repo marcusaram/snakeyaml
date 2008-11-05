7b30ede5838272ad880322255ce9604579d092e1
somov <somov@localhost>
2008-11-05 19:33:28 +0100

    restore MarkedYAMLException

--- src/main/java/org/yaml/snakeyaml/scanner/ScannerException.java
+++ src/main/java/org/yaml/snakeyaml/scanner/ScannerException.java
@@ -3,40 +3,19 @@
  */
 package org.yaml.snakeyaml.scanner;
 
-import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.error.Mark;
+import org.yaml.snakeyaml.error.MarkedYAMLException;
 
 /**
  * @see PyYAML 3.06 for more information
  */
-public class ScannerException extends YAMLException {
-    private static final long serialVersionUID = -1166143510246254991L;
-    private String when;
-    private String what;
-    private String note;
+public class ScannerException extends MarkedYAMLException {
 
-    public ScannerException(final String when, final String what, final String note) {
-        super("ScannerException " + when + " we had this " + what);
-        this.when = when;
-        this.what = what;
-        this.note = note;
+    private static final long serialVersionUID = 4782293188600445954L;
+
+    public ScannerException(String context, Mark contextMark, String problem, Mark problemMark,
+            String note) {
+        super(context, contextMark, problem, problemMark, note);
     }
 
-    public ScannerException(final Throwable thr) {
-        super(thr);
-    }
-
-    public String toString() {
-        final StringBuffer lines = new StringBuffer();
-        if (this.when != null) {
-            lines.append(this.when).append("\n");
-        }
-        if (this.what != null) {
-            lines.append(this.what).append("\n");
-        }
-        if (this.note != null) {
-            lines.append(this.note).append("\n");
-        }
-        lines.append(super.toString());
-        return lines.toString();
-    }
 }
