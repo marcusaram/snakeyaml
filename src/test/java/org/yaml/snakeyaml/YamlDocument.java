8a99b133e698cfd27e464967650840f44261199d
somov <somov@localhost>
2008-11-10 18:32:39 +0100

    import comments from PyYAML to Yaml

--- src/test/java/org/yaml/snakeyaml/YamlDocument.java
+++ src/test/java/org/yaml/snakeyaml/YamlDocument.java
@@ -7,7 +7,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 
-import org.jvyaml.Yaml;
 
 public class YamlDocument {
     public static final String ROOT = "specification/";
