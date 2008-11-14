bd311d99279a58abeb5a5aa6bb5ed33e8c04d0ec
somov <somov@localhost>
2008-11-14 22:11:30 +0100

    ComposerException extends MarkedYAMLException

--- src/main/java/org/yaml/snakeyaml/composer/ComposerException.java
+++ src/main/java/org/yaml/snakeyaml/composer/ComposerException.java
@@ -3,39 +3,17 @@
  */
 package org.yaml.snakeyaml.composer;
 
-import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.error.Mark;
+import org.yaml.snakeyaml.error.MarkedYAMLException;
 
 /**
  * @see PyYAML 3.06 for more information
  */
-public class ComposerException extends YAMLException {
-    private String when;
-    private String what;
-    private String note;
+public class ComposerException extends MarkedYAMLException {
+    private static final long serialVersionUID = 2146314636913113935L;
 
-    public ComposerException(final String when, final String what, final String note) {
-        super("ComposerException " + when + " we had this " + what);
-        this.when = when;
-        this.what = what;
-        this.note = note;
+    protected ComposerException(String context, Mark contextMark, String problem, Mark problemMark) {
+        super(context, contextMark, problem, problemMark);
     }
 
-    public ComposerException(final Throwable thr) {
-        super(thr);
-    }
-
-    public String toString() {
-        final StringBuffer lines = new StringBuffer();
-        if (this.when != null) {
-            lines.append(this.when).append("\n");
-        }
-        if (this.what != null) {
-            lines.append(this.what).append("\n");
-        }
-        if (this.note != null) {
-            lines.append(this.note).append("\n");
-        }
-        lines.append(super.toString());
-        return lines.toString();
-    }
 }
