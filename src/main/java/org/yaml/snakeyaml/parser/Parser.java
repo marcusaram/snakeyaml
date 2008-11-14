fec946608d0fe12ced55f6db4e3cac48f873e372
somov <somov@localhost>
2008-11-14 09:45:50 +0100

    further clean Parser interface - leave only 2 methods out of 7

--- src/main/java/org/yaml/snakeyaml/parser/Parser.java
+++ src/main/java/org/yaml/snakeyaml/parser/Parser.java
@@ -4,19 +4,13 @@
 package org.yaml.snakeyaml.parser;
 
 import org.yaml.snakeyaml.events.Event;
-import org.yaml.snakeyaml.tokens.Token;
 
 /**
  * @see PyYAML 3.06 for more information
  */
 public interface Parser {
-    boolean checkEvent(final Class<Token>[] choices);
-
     Event peekEvent();
 
     Event getEvent();
 
-    void parseStream();
-
-    Event parseStreamNext();
 }
