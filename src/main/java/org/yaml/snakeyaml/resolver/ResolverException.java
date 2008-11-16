4e9f212893cf8913d36ac9aec97748f2725c4fa2
Andrey Somov <py4fun@gmail.com>
2008-11-16 11:22:50 +0100

    add generated id

--- src/main/java/org/yaml/snakeyaml/resolver/ResolverException.java
+++ src/main/java/org/yaml/snakeyaml/resolver/ResolverException.java
@@ -9,6 +9,8 @@ import org.yaml.snakeyaml.error.YAMLException;
  * @see PyYAML 3.06 for more information
  */
 class ResolverException extends YAMLException {
+    private static final long serialVersionUID = -4199504387416260972L;
+
     public ResolverException(final String msg) {
         super(msg);
     }
