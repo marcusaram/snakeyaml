9027ac2545ef81975c679197d0b083c6405485b1
somov <somov@localhost>
2008-11-25 15:20:51 +0100

    reduce representer interface

--- src/main/java/org/yaml/snakeyaml/representer/Representer.java
+++ src/main/java/org/yaml/snakeyaml/representer/Representer.java
@@ -4,20 +4,10 @@
 package org.yaml.snakeyaml.representer;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-import org.yaml.snakeyaml.nodes.Node;
 
 /**
  * @see PyYAML 3.06 for more information
  */
 public interface Representer {
     void represent(final Object data) throws IOException;
-
-    Node scalar(final String tag, final String value, char style) throws IOException;
-
-    Node seq(final String tag, final List sequence, final boolean flowStyle) throws IOException;
-
-    Node map(final String tag, final Map mapping, final boolean flowStyle) throws IOException;
 }
