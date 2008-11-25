e3e8cb8d1a4ba25341f4740558b47b0f7519e0f8
somov <somov@localhost>
2008-11-25 14:06:34 +0100

    remove DefaultYAMLFactory

--- src/main/java/org/jvyaml/YAMLFactory.java
+++ src/main/java/org/jvyaml/YAMLFactory.java
@@ -8,33 +8,60 @@ import java.io.Writer;
 
 import org.yaml.snakeyaml.YamlConfig;
 import org.yaml.snakeyaml.composer.Composer;
+import org.yaml.snakeyaml.composer.ComposerImpl;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.constructor.ConstructorImpl;
 import org.yaml.snakeyaml.emitter.Emitter;
+import org.yaml.snakeyaml.emitter.EmitterImpl;
 import org.yaml.snakeyaml.parser.Parser;
+import org.yaml.snakeyaml.parser.ParserImpl;
 import org.yaml.snakeyaml.representer.Representer;
+import org.yaml.snakeyaml.representer.RepresenterImpl;
 import org.yaml.snakeyaml.resolver.Resolver;
+import org.yaml.snakeyaml.resolver.ResolverImpl;
 import org.yaml.snakeyaml.scanner.Scanner;
+import org.yaml.snakeyaml.scanner.ScannerImpl;
 import org.yaml.snakeyaml.serializer.Serializer;
+import org.yaml.snakeyaml.serializer.SerializerImpl;
 
 /**
  * @see PyYAML 3.06 for more information
  */
-public interface YAMLFactory {
-    Scanner createScanner(final String io);
+public class YAMLFactory {
+    public Scanner createScanner(final String io) {
+        return new ScannerImpl(new org.yaml.snakeyaml.reader.Reader(io));
+    }
 
-    Scanner createScanner(final InputStream io);
+    public Scanner createScanner(final InputStream io) {
+        return new ScannerImpl(new org.yaml.snakeyaml.reader.Reader(io));
+    }
 
-    Parser createParser(final Scanner scanner);
+    public Parser createParser(final Scanner scanner) {
+        return new ParserImpl(scanner);
+    }
 
-    Resolver createResolver();
+    public Resolver createResolver() {
+        return new ResolverImpl();
+    }
 
-    Composer createComposer(final Parser parser, final Resolver resolver);
+    public Composer createComposer(final Parser parser, final Resolver resolver) {
+        return new ComposerImpl(parser, resolver);
+    }
 
-    Constructor createConstructor(final Composer composer);
+    public Constructor createConstructor(final Composer composer) {
+        return new ConstructorImpl(composer);
+    }
 
-    Emitter createEmitter(final Writer output, final YamlConfig cfg);
+    public Emitter createEmitter(final Writer output, final YamlConfig cfg) {
+        return new EmitterImpl(output, cfg);
+    }
 
-    Serializer createSerializer(final Emitter emitter, final Resolver resolver, final YamlConfig cfg);
+    public Serializer createSerializer(final Emitter emitter, final Resolver resolver,
+            final YamlConfig cfg) {
+        return new SerializerImpl(emitter, resolver, cfg);
+    }
 
-    Representer createRepresenter(final Serializer serializer, final YamlConfig cfg);
+    public Representer createRepresenter(final Serializer serializer, final YamlConfig cfg) {
+        return new RepresenterImpl(serializer, cfg);
+    }
 }
