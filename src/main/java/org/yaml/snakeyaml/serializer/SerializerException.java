49689e20252c1f5e05d809bd5eedec5cfcdbf20a
somov <somov@localhost>
2008-11-25 11:55:32 +0100

    add generated ID to SerializerException

--- src/main/java/org/yaml/snakeyaml/serializer/SerializerException.java
+++ src/main/java/org/yaml/snakeyaml/serializer/SerializerException.java
@@ -9,11 +9,9 @@ import org.yaml.snakeyaml.error.YAMLException;
  * @see PyYAML 3.06 for more information
  */
 public class SerializerException extends YAMLException {
-    public SerializerException(final String msg) {
-        super(msg);
-    }
+    private static final long serialVersionUID = 2632638197498912433L;
 
-    public SerializerException(final Throwable thr) {
-        super(thr);
+    public SerializerException(String message) {
+        super(message);
     }
 }
