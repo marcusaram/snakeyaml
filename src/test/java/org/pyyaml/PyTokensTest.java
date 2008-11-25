39664659c19bda11bb85c61dcf330bcc50d2268a
somov <somov@localhost>
2008-11-25 18:58:19 +0100

    import tokens test from PyYAML

--- src/test/java/org/pyyaml/PyTokensTest.java
+++ src/test/java/org/pyyaml/PyTokensTest.java
@@ -4,13 +4,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.yaml.snakeyaml.reader.Reader;
 import org.yaml.snakeyaml.scanner.Scanner;
 import org.yaml.snakeyaml.scanner.ScannerImpl;
+import org.yaml.snakeyaml.tokens.AliasToken;
+import org.yaml.snakeyaml.tokens.AnchorToken;
+import org.yaml.snakeyaml.tokens.BlockEndToken;
+import org.yaml.snakeyaml.tokens.BlockEntryToken;
+import org.yaml.snakeyaml.tokens.BlockMappingStartToken;
+import org.yaml.snakeyaml.tokens.BlockSequenceStartToken;
+import org.yaml.snakeyaml.tokens.DirectiveToken;
+import org.yaml.snakeyaml.tokens.DocumentEndToken;
+import org.yaml.snakeyaml.tokens.DocumentStartToken;
+import org.yaml.snakeyaml.tokens.FlowEntryToken;
+import org.yaml.snakeyaml.tokens.FlowMappingEndToken;
+import org.yaml.snakeyaml.tokens.FlowMappingStartToken;
+import org.yaml.snakeyaml.tokens.FlowSequenceEndToken;
+import org.yaml.snakeyaml.tokens.FlowSequenceStartToken;
+import org.yaml.snakeyaml.tokens.KeyToken;
+import org.yaml.snakeyaml.tokens.ScalarToken;
+import org.yaml.snakeyaml.tokens.StreamEndToken;
+import org.yaml.snakeyaml.tokens.StreamStartToken;
+import org.yaml.snakeyaml.tokens.TagToken;
 import org.yaml.snakeyaml.tokens.Token;
+import org.yaml.snakeyaml.tokens.ValueToken;
 
 /**
  * @see imported from PyYAML
@@ -37,6 +59,78 @@ public class PyTokensTest extends PyImportTest {
             }
         }
         return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testTokens() throws FileNotFoundException {
+        Map<Class, String> replaces = new HashMap<Class, String>();
+        replaces.put(DirectiveToken.class, "%");
+        replaces.put(DocumentStartToken.class, "---");
+        replaces.put(DocumentEndToken.class, "...");
+        replaces.put(AliasToken.class, "*");
+        replaces.put(AnchorToken.class, "&");
+        replaces.put(TagToken.class, "!");
+        replaces.put(ScalarToken.class, "_");
+        replaces.put(BlockSequenceStartToken.class, "[[");
+        replaces.put(BlockMappingStartToken.class, "{{");
+        replaces.put(BlockEndToken.class, "]}");
+        replaces.put(FlowSequenceStartToken.class, "[");
+        replaces.put(FlowSequenceEndToken.class, "]");
+        replaces.put(FlowMappingStartToken.class, "{");
+        replaces.put(FlowMappingEndToken.class, "}");
+        replaces.put(BlockEntryToken.class, ",");
+        replaces.put(FlowEntryToken.class, ",");
+        replaces.put(KeyToken.class, "?");
+        replaces.put(ValueToken.class, ":");
+        //
+        File[] tokensFiles = getStreamsByExtension(".tokens");
+        assertTrue("No test files found.", tokensFiles.length > 0);
+        for (int i = 0; i < tokensFiles.length; i++) {
+            String name = tokensFiles[i].getName();
+            int position = name.lastIndexOf('.');
+            String dataName = name.substring(0, position) + ".data";
+            if (skip(tokensFiles[i].getName())) {
+                continue;
+            }
+            //
+            String tokenFileData = getResource(name);
+            String[] split = tokenFileData.split("\\s+");
+            List<String> tokens2 = new LinkedList<String>();
+            for (int j = 0; j < split.length; j++) {
+                tokens2.add(split[j]);
+            }
+            //
+            List<String> tokens1 = new LinkedList<String>();
+            Reader reader = new Reader(new FileInputStream(getFileByName(dataName)));
+            Scanner scanner = new ScannerImpl(reader);
+            try {
+                while (scanner.checkToken(new ArrayList<Class>())) {
+                    Token token = scanner.getToken();
+                    if (!(token instanceof StreamStartToken || token instanceof StreamEndToken)) {
+                        String replacement = replaces.get(token.getClass());
+                        tokens1.add(replacement);
+                    }
+                }
+                // System.out.println("File name: \n" +
+                // tokensFiles[i].getName());
+                // Iterator iter = tokens2.iterator();
+                // for (String string : tokens1) {
+                // String str2 = (String) iter.next();
+                // System.out.println(string + "=" + str2);
+                // }
+                assertEquals(tokenFileData, tokens1.size(), tokens2.size());
+                assertEquals(tokens1, tokens2);
+            } catch (RuntimeException e) {
+                System.out.println("File name: \n" + tokensFiles[i].getName());
+                String data = getResource(tokensFiles[i].getName());
+                System.out.println("Data: \n" + data);
+                System.out.println("Tokens:");
+                for (String token : tokens1) {
+                    System.out.println(token);
+                }
+                fail("Cannot scan: " + tokensFiles[i]);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
