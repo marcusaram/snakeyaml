b4d8d7d36fe2fcce0c813417c51a5f0a6c3ba827
somov <somov@localhost>
2008-11-25 14:08:58 +0100

    add generated serialVersionUID

--- src/main/java/org/yaml/snakeyaml/representer/RepresenterException.java
+++ src/main/java/org/yaml/snakeyaml/representer/RepresenterException.java
@@ -9,11 +9,9 @@ import org.yaml.snakeyaml.error.YAMLException;
  * @see PyYAML 3.06 for more information
  */
 public class RepresenterException extends YAMLException {
+    private static final long serialVersionUID = -7594852637893412485L;
+
     public RepresenterException(final String msg) {
         super(msg);
-    }
-
-    public RepresenterException(final Throwable thr) {
-        super(thr);
     }
 }
