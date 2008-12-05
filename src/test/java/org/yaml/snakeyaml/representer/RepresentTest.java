aed27992d71b6419d5bd1d2b5640c3bc3c73ea23
somov <somov@localhost>
2008-12-05 11:19:44 +0100

    apply a test for custom Representer

--- src/test/java/org/yaml/snakeyaml/representer/RepresentTest.java
+++ src/test/java/org/yaml/snakeyaml/representer/RepresentTest.java
@@ -2,17 +2,18 @@ package org.yaml.snakeyaml.representer;
 
 import junit.framework.TestCase;
 
+import org.yaml.snakeyaml.Dumper;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.nodes.Node;
-import org.yaml.snakeyaml.representer.Representer;
 
 public class RepresentTest extends TestCase {
 
     public void testCustomRepresenter() {
-        Yaml yaml = new Yaml();
-        // yaml.addRepresenter(Dice.class, new CoinRepresenter());
+        Dumper dumper = new Dumper(new MyRepresenter(), new DumperOptions());
+        Yaml yaml = new Yaml(dumper);
         String output = yaml.dump(new Dice("A", 1));
-        // assertEquals("!!Dice 'Ad1'\n", output);
+        assertEquals("!!Dice 'Ad1'\n", output);
     }
 
     class Dice {
