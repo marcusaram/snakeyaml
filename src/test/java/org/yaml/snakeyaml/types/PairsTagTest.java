9f3090f2db17c1de4ea094ff4210409bf67eaa70
somov <somov@localhost>
2008-12-08 10:28:43 +0100

    pairs tag works properly

--- src/test/java/org/yaml/snakeyaml/types/PairsTagTest.java
+++ src/test/java/org/yaml/snakeyaml/types/PairsTagTest.java
@@ -16,25 +16,44 @@ public class PairsTagTest extends AbstractTest {
 
     @SuppressWarnings("unchecked")
     public void testPairs() throws IOException {
-        try {
-            YamlDocument document = new YamlDocument("types/pairs.yaml");
-            Map<String, List<Map<String, String>>> map = (Map<String, List<Map<String, String>>>) document
-                    .getNativeData();
-            assertEquals(2, map.size());
-            List<Map<String, String>> list1 = (List<Map<String, String>>) map.get("Block tasks");
-            assertEquals(4, list1.size());
-            assertEquals("with team.", list1.get(0).get("meeting"));
-            assertEquals("with boss.", list1.get(1).get("meeting"));
-            assertEquals("lunch.", list1.get(2).get("break"));
-            assertEquals("with client.", list1.get(3).get("meeting"));
-            //
-            List<Map<String, String>> list2 = (List<Map<String, String>>) map.get("Flow tasks");
-            assertEquals(2, list2.size());
-            assertEquals("with team", list2.get(0).get("meeting"));
-            assertEquals("with boss", list2.get(1).get("meeting"));
-        } catch (RuntimeException e) {
-            // TODO pairs is not yet properly implemented
-        }
-    }
+        YamlDocument document = new YamlDocument("types/pairs.yaml", false);
+        Map<String, List<String[]>> map = (Map<String, List<String[]>>) document.getNativeData();
+        assertEquals(2, map.size());
+        List<String[]> list1 = (List<String[]>) map.get("Block tasks");
+        assertEquals(4, list1.size());
+        Object[] tuple1 = list1.get(0);
+        assertEquals(2, tuple1.length);
+        assertEquals("meeting", tuple1[0]);
+        assertEquals("with team.", tuple1[1]);
+        //
 
+        Object[] tuple2 = list1.get(1);
+        assertEquals(2, tuple2.length);
+        assertEquals("meeting", tuple2[0]);
+        assertEquals("with boss.", tuple2[1]);
+        //
+
+        Object[] tuple3 = list1.get(2);
+        assertEquals(2, tuple3.length);
+        assertEquals("break", tuple3[0]);
+        assertEquals("lunch.", tuple3[1]);
+        //
+
+        Object[] tuple4 = list1.get(3);
+        assertEquals(2, tuple4.length);
+        assertEquals("meeting", tuple4[0]);
+        assertEquals("with client.", tuple4[1]);
+        //
+        List<String[]> list2 = (List<String[]>) map.get("Flow tasks");
+        assertEquals(2, list2.size());
+        Object[] tuple2_1 = list2.get(0);
+        assertEquals(2, tuple2_1.length);
+        assertEquals("meeting", tuple2_1[0]);
+        assertEquals("with team", tuple2_1[1]);
+        //
+        Object[] tuple2_2 = list2.get(1);
+        assertEquals(2, tuple2_2.length);
+        assertEquals("meeting", tuple2_2[0]);
+        assertEquals("with boss", tuple2_2[1]);
+    }
 }
