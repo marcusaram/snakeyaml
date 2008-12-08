dd0622b4c306c251ab0488e94f4f3f3bd3e2fba1
somov <somov@localhost>
2008-12-08 09:16:43 +0100

    move Java construction to Constructor

--- src/main/java/org/yaml/snakeyaml/constructor/Constructor.java
+++ src/main/java/org/yaml/snakeyaml/constructor/Constructor.java
@@ -11,16 +11,42 @@ import org.yaml.snakeyaml.nodes.Node;
 public class Constructor extends SafeConstructor {
 
     public Constructor() {
-        super();
-        // TODO this.yamlMultiConstructors.put("!java/object:", new
-        // ConstuctYamlNull());
+        this.yamlConstructors.put("tag:yaml.org,2002:java/object", new ConstuctYamlObject());
     }
 
-    private class ConstuctJavaBean implements Construct {
+    private class ConstuctYamlObject implements Construct {
         public Object construct(Node node) {
-            constructScalar(node);
-            return null;
+            // Object outp = null;
+            // try {
+            // final Class cl = Class.forName(pref);
+            // outp = cl.newInstance();
+            // final Map values = (Map) ctor.constructMapping(node);
+            // java.lang.reflect.Method[] ems = cl.getMethods();
+            // for (final Iterator iter = values.keySet().iterator();
+            // iter.hasNext();) {
+            // final Object key = iter.next();
+            // final Object value = values.get(key);
+            // final String keyName = key.toString();
+            // final String mName = "set" +
+            // Character.toUpperCase(keyName.charAt(0))
+            // + keyName.substring(1);
+            // for (int i = 0, j = ems.length; i < j; i++) {
+            // if (ems[i].getName().equals(mName)
+            // && ems[i].getParameterTypes().length == 1) {
+            // ems[i].invoke(outp, new Object[] { fixValue(value, ems[i]
+            // .getParameterTypes()[0]) });
+            // break;
+            // }
+            // }
+            // }
+            // } catch (final Exception e) {
+            // throw new
+            // YAMLException("Can't construct a java object from class " + pref
+            // + ": "
+            // + e.toString());
+            // }
+            // return outp;
+            throw new UnsupportedOperationException("GGGGGGGGGGGGGGG RRRRRRRRRRRRR");
         }
     }
-
 }
--- src/main/java/org/yaml/snakeyaml/constructor/SafeConstructor.java
+++ src/main/java/org/yaml/snakeyaml/constructor/SafeConstructor.java
@@ -384,7 +384,6 @@ public class SafeConstructor extends BaseConstructor {
 
     private class ConstuctYamlSet implements Construct {
         public Object construct(Node node) {
-            // TODO why generator ???
             Map<Object, Object> value = constructMapping((MappingNode) node);
             return value.keySet();
         }
@@ -406,43 +405,6 @@ public class SafeConstructor extends BaseConstructor {
     private class ConstuctYamlMap implements Construct {
         public Object construct(Node node) {
             return constructMapping((MappingNode) node);
-        }
-    }
-
-    // TODO milticonstructor
-    private class ConstuctYamlObject implements Construct {
-        public Object construct(Node node) {
-            // Object outp = null;
-            // try {
-            // final Class cl = Class.forName(pref);
-            // outp = cl.newInstance();
-            // final Map values = (Map) ctor.constructMapping(node);
-            // java.lang.reflect.Method[] ems = cl.getMethods();
-            // for (final Iterator iter = values.keySet().iterator();
-            // iter.hasNext();) {
-            // final Object key = iter.next();
-            // final Object value = values.get(key);
-            // final String keyName = key.toString();
-            // final String mName = "set" +
-            // Character.toUpperCase(keyName.charAt(0))
-            // + keyName.substring(1);
-            // for (int i = 0, j = ems.length; i < j; i++) {
-            // if (ems[i].getName().equals(mName)
-            // && ems[i].getParameterTypes().length == 1) {
-            // ems[i].invoke(outp, new Object[] { fixValue(value, ems[i]
-            // .getParameterTypes()[0]) });
-            // break;
-            // }
-            // }
-            // }
-            // } catch (final Exception e) {
-            // throw new
-            // YAMLException("Can't construct a java object from class " + pref
-            // + ": "
-            // + e.toString());
-            // }
-            // return outp;
-            throw new UnsupportedOperationException();
         }
     }
 
