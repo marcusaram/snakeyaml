cd81d0c0c932362eef8869d6c28ba1bb3084c224
somov <somov@localhost>
2008-12-17 15:45:44 +0100

    provide dump examples

--- src/test/java/examples/DumpExampleTest.java
+++ src/test/java/examples/DumpExampleTest.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 public class DumpExampleTest extends TestCase {
@@ -40,11 +41,85 @@ public class DumpExampleTest extends TestCase {
 
     public void testDumpMany() {
         List<Integer> docs = new LinkedList<Integer>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i < 4; i++) {
             docs.add(i);
         }
+        DumperOptions options = new DumperOptions();
+        options.explicitStart(true);
+        Yaml yaml = new Yaml(options);
+        System.out.println(yaml.dump(docs));
+        System.out.println(yaml.dumpAll(docs.iterator()));
+    }
+
+    public void testDumpCustomJavaClass() {
+        Hero hero = new Hero("Galain Ysseleg", -3, 2);
         Yaml yaml = new Yaml();
-        String output = yaml.dumpAll(docs.iterator());
+        String output = yaml.dump(hero);
+        System.out.println(output);
+        assertEquals("!<examples.Hero> {hp: -3, name: Galain Ysseleg, sp: 2}\n", output);
+    }
+
+    public void testDumperOptions() {
+        List<Integer> data = new LinkedList<Integer>();
+        for (int i = 0; i < 50; i++) {
+            data.add(i);
+        }
+        Yaml yaml = new Yaml();
+        String output = yaml.dump(data);
+        System.out.println(output);
+        assertTrue(output.contains("[0, 1, 2, 3, 4, 5, 6, 7, 8"));
+        //
+        DumperOptions options = new DumperOptions();
+        options.setWidth(50);
+        options.setIndent(4);
+        yaml = new Yaml(options);
+        output = yaml.dump(data);
         System.out.println(output);
     }
+
+    public void testDumperOptionsCanonical() {
+        List<Integer> data = new LinkedList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            data.add(i);
+        }
+        DumperOptions options = new DumperOptions();
+        options.setCanonical(true);
+        Yaml yaml = new Yaml(options);
+        String output = yaml.dump(data);
+        System.out.println(output);
+        assertTrue(output.contains("---"));
+        assertTrue(output.contains("!!seq ["));
+        assertTrue(output.contains("!!int \"3\","));
+    }
+
+    public void testDumperOptionsFlowStyle() {
+        List<Integer> data = new LinkedList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            data.add(i);
+        }
+        DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(Boolean.FALSE);
+        Yaml yaml = new Yaml(options);
+        String output = yaml.dump(data);
+        System.out.println(output);
+        assertTrue(output.contains("- 0\n"));
+        assertTrue(output.contains("- 1\n"));
+        assertTrue(output.contains("- 4\n"));
+    }
+
+    public void testDumperOptionsStyle() {
+        List<Integer> data = new LinkedList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            data.add(i);
+        }
+        DumperOptions options = new DumperOptions();
+        options.setDefaultStyle('"');
+        Yaml yaml = new Yaml(options);
+        String output = yaml.dump(data);
+        System.out.println(output);
+        assertTrue(output.contains("- !!int \"0\""));
+        assertTrue(output.contains("- !!int \"1\""));
+        assertTrue(output.contains("- !!int \"4\""));
+    }
+
 }
