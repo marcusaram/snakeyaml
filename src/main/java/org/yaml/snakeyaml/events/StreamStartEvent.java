772ad3888bb9be32e40c5114960224732a5df98f
somov <somov@localhost>
2008-12-19 16:23:50 +0100

    improve test coverage

--- src/main/java/org/yaml/snakeyaml/events/StreamStartEvent.java
+++ src/main/java/org/yaml/snakeyaml/events/StreamStartEvent.java
@@ -3,22 +3,14 @@
  */
 package org.yaml.snakeyaml.events;
 
-import java.nio.charset.Charset;
-
 import org.yaml.snakeyaml.error.Mark;
 
 /**
  * @see <a href="http://pyyaml.org/wiki/PyYAML">PyYAML</a> for more information
  */
 public class StreamStartEvent extends Event {
-    private Charset encoding;
 
     public StreamStartEvent(Mark startMark, Mark endMark) {
         super(startMark, endMark);
-        this.encoding = Charset.forName("UTF-8");
-    }
-
-    public Charset getEncoding() {
-        return encoding;
     }
 }
