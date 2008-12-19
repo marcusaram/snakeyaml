5322757264bb0103ab06ac5839d7dac4c431fe8d
somov <somov@localhost>
2008-12-19 17:54:18 +0100

    improve test coverage for Dumper

--- src/test/java/org/yaml/snakeyaml/DumperTest.java
+++ src/test/java/org/yaml/snakeyaml/DumperTest.java
@@ -1,5 +1,7 @@
 package org.yaml.snakeyaml;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -45,4 +47,62 @@ public class DumperTest extends TestCase {
         assertEquals("- !!int '0'\n- !!int '1'\n- !!int '2'\n", output);
     }
 
+    public void testDumpException() {
+        Yaml yaml = new Yaml();
+        Writer writer = new ExceptionWriter1();
+        try {
+            yaml.dump("aaa1234567890", writer);
+            System.out.println(writer.toString());
+            fail("Exception must be thrown.");
+        } catch (Exception e) {
+            assertEquals("java.io.IOException: write test failure.", e.getMessage());
+        }
+    }
+
+    private class ExceptionWriter1 extends Writer {
+        @Override
+        public void write(String str) throws IOException {
+            throw new IOException("write test failure.");
+        }
+
+        @Override
+        public void close() throws IOException {
+        }
+
+        @Override
+        public void flush() throws IOException {
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+        }
+    }
+
+    public void testDumpCloseException() {
+        Yaml yaml = new Yaml();
+        Writer writer = new ExceptionWriter2();
+        try {
+            yaml.dump("aaa1234567890", writer);
+            System.out.println(writer.toString());
+            fail("Exception must be thrown.");
+        } catch (Exception e) {
+            assertEquals("java.io.IOException: close test failure.", e.getMessage());
+        }
+    }
+
+    private class ExceptionWriter2 extends Writer {
+        @Override
+        public void flush() throws IOException {
+            throw new IOException("close test failure.");
+        }
+
+        @Override
+        public void close() throws IOException {
+            throw new IOException("close test failure.");
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+        }
+    }
 }
