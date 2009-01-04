4de50875d9f7add457fecc4b265595c5de041523
somov <somov@localhost>
2009-01-04 09:40:29 +0100

    Add example of defining a custom List and Map implementations

---
+++ src/test/java/examples/CustomListExampleTest.java
@@ -0,0 +1,27 @@
+package examples;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.yaml.snakeyaml.Loader;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+public class CustomListExampleTest extends TestCase {
+    public void testList() throws IOException {
+        Loader loader = new Loader(new CustomConstructor());
+        Yaml yaml = new Yaml(loader);
+        List data = (List) yaml.load("[1, 2, 3]");
+        assertTrue(data instanceof ArrayList);
+    }
+
+    class CustomConstructor extends Constructor {
+        @Override
+        protected List<Object> createDefaultList(int initSize) {
+            return new ArrayList<Object>(initSize);
+        }
+    }
+}
