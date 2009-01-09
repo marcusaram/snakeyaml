69fc35086f1acc3dcef93a0f02dd5ba6fd65ed6a
somov <somov@localhost>
2009-01-09 14:49:06 +0100

    update test

--- src/test/java/org/yaml/snakeyaml/constructor/BeanConstructorTest.java
+++ src/test/java/org/yaml/snakeyaml/constructor/BeanConstructorTest.java
@@ -1,21 +1,31 @@
 package org.yaml.snakeyaml.constructor;
 
+import java.io.IOException;
+
 import junit.framework.TestCase;
 
 import org.yaml.snakeyaml.Loader;
+import org.yaml.snakeyaml.Util;
 import org.yaml.snakeyaml.Yaml;
 
 public class BeanConstructorTest extends TestCase {
 
-    public void testPrimitivesConstructor() {
+    public void testPrimitivesConstructor() throws IOException {
         Loader loader = new Loader(new BeanConstructor(TestBean1.class));
         Yaml yaml = new Yaml(loader);
-        String document = "integer: 5\ntext: the text\nintPrimitive: 17\nid: 13";
+        String document = Util.getLocalResource("constructor/test-primitives1.yaml");
+        System.out.println(document);
         TestBean1 result = (TestBean1) yaml.load(document);
         assertNotNull(result);
-        assertEquals("the text", result.getText());
+        assertEquals(new Byte((byte) 1), result.getByteClass());
+        assertEquals((byte) -3, result.getBytePrimitive());
+        assertEquals(new Short((short) 0), result.getShortClass());
+        assertEquals((short) -13, result.getShortPrimitive());
         assertEquals(new Integer(5), result.getInteger());
         assertEquals(17, result.getIntPrimitive());
+        assertEquals("the text", result.getText());
         assertEquals("13", result.getId());
+        assertEquals(new Long(11111111111L), result.getLongClass());
+        assertEquals(9999999999L, result.getLongPrimitive());
     }
 }
--- src/test/java/org/yaml/snakeyaml/constructor/TestBean1.java
+++ src/test/java/org/yaml/snakeyaml/constructor/TestBean1.java
@@ -6,8 +6,14 @@ package org.yaml.snakeyaml.constructor;
 public class TestBean1 {
     private String text;
     private String id;
+    private Byte byteClass;
+    private byte bytePrimitive;
+    private Short shortClass;
+    private short shortPrimitive;
     private Integer integer;
     private int intPrimitive;
+    private Long longClass;
+    private long longPrimitive;
 
     public String getText() {
         return text;
@@ -39,5 +45,53 @@ public class TestBean1 {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public Byte getByteClass() {
+        return byteClass;
+    }
+
+    public void setByteClass(Byte byteClass) {
+        this.byteClass = byteClass;
+    }
+
+    public byte getBytePrimitive() {
+        return bytePrimitive;
+    }
+
+    public void setBytePrimitive(byte bytePrimitive) {
+        this.bytePrimitive = bytePrimitive;
+    }
+
+    public Short getShortClass() {
+        return shortClass;
+    }
+
+    public void setShortClass(Short shortClass) {
+        this.shortClass = shortClass;
+    }
+
+    public short getShortPrimitive() {
+        return shortPrimitive;
+    }
+
+    public void setShortPrimitive(short shortPrimitive) {
+        this.shortPrimitive = shortPrimitive;
+    }
+
+    public Long getLongClass() {
+        return longClass;
+    }
+
+    public void setLongClass(Long longClass) {
+        this.longClass = longClass;
+    }
+
+    public long getLongPrimitive() {
+        return longPrimitive;
+    }
+
+    public void setLongPrimitive(long longPrimitive) {
+        this.longPrimitive = longPrimitive;
     }
 }
