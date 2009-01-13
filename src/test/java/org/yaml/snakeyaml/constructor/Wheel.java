4369edfebfe19afa636b4dcf4146635041850a48
somov <somov@localhost>
2009-01-13 18:58:24 +0100

    Construct List as a JavaBean property

---
+++ src/test/java/org/yaml/snakeyaml/constructor/Wheel.java
@@ -0,0 +1,14 @@
+package org.yaml.snakeyaml.constructor;
+
+public class Wheel {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
