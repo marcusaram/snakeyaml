334dafc32c8bd008f038e2eedf62e0cb1e5199c0
somov <somov@localhost>
2009-02-09 10:53:59 +0100

    add testDefaultFlowStyle

--- src/test/java/org/yaml/snakeyaml/DumperOptionsTest.java
+++ src/test/java/org/yaml/snakeyaml/DumperOptionsTest.java
@@ -1,9 +1,11 @@
 package org.yaml.snakeyaml;
 
-import junit.framework.TestCase;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
+import junit.framework.TestCase;
 
 public class DumperOptionsTest extends TestCase {
 
@@ -23,5 +25,48 @@ public class DumperOptionsTest extends TestCase {
         options.setDefaultStyle(null);
         yaml = new Yaml(options);
         assertEquals("'123'\n", yaml.dump("123"));
+    }
+
+    public void testDefaultFlowStyle() {
+        Yaml yaml = new Yaml();
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertEquals("[1, 2, 3]\n", yaml.dump(list));
+        //
+        DumperOptions options = new DumperOptions();
+        options = new DumperOptions();
+        options.setDefaultFlowStyle(true);
+        yaml = new Yaml(options);
+        assertEquals("[1, 2, 3]\n", yaml.dump(list));
+        //
+        options = new DumperOptions();
+        options.setDefaultFlowStyle(false);
+        yaml = new Yaml(options);
+        assertEquals("- 1\n- 2\n- 3\n", yaml.dump(list));
+    }
+
+    public void testDefaultFlowStyleNested() {
+        Yaml yaml = new Yaml();
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("a", "b");
+        map.put("c", list);
+        assertEquals("a: b\nc: [1, 2, 3]\n", yaml.dump(map));
+        //
+        DumperOptions options = new DumperOptions();
+        options = new DumperOptions();
+        options.setDefaultFlowStyle(true);
+        yaml = new Yaml(options);
+        assertEquals("{a: b, c: [1, 2, 3]}\n", yaml.dump(map));
+        //
+        options = new DumperOptions();
+        options.setDefaultFlowStyle(false);
+        yaml = new Yaml(options);
+        assertEquals("a: b\nc:\n- 1\n- 2\n- 3\n", yaml.dump(map));
     }
 }
