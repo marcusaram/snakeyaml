5479453d65d5711cb1f38d708f45677c38a9767c
somov <somov@localhost>
2009-02-17 18:30:51 +0100

    Enum is parsed as a scalar node or as a JavaBean property

---
+++ src/test/java/org/yaml/snakeyaml/EnumBean.java
@@ -0,0 +1,25 @@
+/*
+ * See LICENSE file in distribution for copyright and licensing information.
+ */
+package org.yaml.snakeyaml;
+
+public class EnumBean {
+    private int id;
+    private Suit suit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+}
