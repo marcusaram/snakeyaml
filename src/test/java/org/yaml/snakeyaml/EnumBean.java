bb910c75df885ee903185d85948cac5b843d4a85
somov <somov@localhost>
2009-02-18 00:57:24 +0100

    add more tests for Enum

--- src/test/java/org/yaml/snakeyaml/EnumBean.java
+++ src/test/java/org/yaml/snakeyaml/EnumBean.java
@@ -3,9 +3,20 @@
  */
 package org.yaml.snakeyaml;
 
+import java.util.LinkedHashMap;
+
 public class EnumBean {
     private int id;
     private Suit suit;
+    private LinkedHashMap<Suit, Integer> map = new LinkedHashMap<Suit, Integer>();
+
+    public LinkedHashMap<Suit, Integer> getMap() {
+        return map;
+    }
+
+    public void setMap(LinkedHashMap<Suit, Integer> map) {
+        this.map = map;
+    }
 
     public int getId() {
         return id;
