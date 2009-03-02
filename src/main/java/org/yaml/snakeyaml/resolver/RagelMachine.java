bcefdaefa2be423012d7665be975aea8a3b2a699
somov <somov@localhost>
2009-03-02 16:42:43 +0100

    add merge, null, value, int to Ragel

--- src/main/java/org/yaml/snakeyaml/resolver/RagelMachine.java
+++ src/main/java/org/yaml/snakeyaml/resolver/RagelMachine.java
@@ -5,7 +5,7 @@
 package org.yaml.snakeyaml.resolver;
 
 public class RagelMachine {
-    // line 17 "RagelMachine.rl"
+    // line 32 "RagelMachine.rl"
 
     
     
@@ -13,7 +13,7 @@ public class RagelMachine {
 private static byte[] init__snakeyaml_actions_0()
 {
 	return new byte [] {
-	    0,    1,    0
+	    0,    1,    0,    1,    1,    1,    2,    1,    3,    1,    4
 	};
 }
 
@@ -23,9 +23,10 @@ private static final byte _snakeyaml_actions[] = init__snakeyaml_actions_0();
 private static byte[] init__snakeyaml_key_offsets_0()
 {
 	return new byte [] {
-	    0,    0,   10,   12,   13,   14,   15,   16,   17,   18,   20,   24,
-	   25,   26,   28,   29,   30,   32,   33,   34,   35,   36,   38,   39,
-	   40
+	    0,    0,   19,   22,   25,   33,   37,   38,   40,   41,   42,   43,
+	   44,   45,   46,   50,   51,   52,   53,   54,   58,   59,   60,   62,
+	   63,   64,   66,   67,   68,   69,   71,   73,   74,   75,   75,   80,
+	   83,   86,   94,   99,  102,  103,  103,  103
 	};
 }
 
@@ -35,10 +36,15 @@ private static final byte _snakeyaml_key_offsets[] = init__snakeyaml_key_offsets
 private static char[] init__snakeyaml_trans_keys_0()
 {
 	return new char [] {
-	   70,   78,   79,   84,   89,  102,  110,  111,  116,  121,   65,   97,
-	   76,   83,   69,  108,  115,  101,   79,  111,   70,   78,  102,  110,
-	   70,  102,   82,  114,   85,  117,   69,  101,   83,  115,   97,  111,
-	  102,  110,  114,  101,    0
+	   32,   43,   45,   48,   60,   61,   70,   78,   79,   84,   89,  102,
+	  110,  111,  116,  121,  126,   49,   57,   48,   49,   57,   95,   48,
+	   49,   44,   95,   48,   57,   65,   70,   97,  102,   48,   53,   54,
+	   57,   60,   65,   97,   76,   83,   69,  108,  115,  101,   79,   85,
+	  111,  117,   76,   76,  108,  108,   70,   78,  102,  110,   70,  102,
+	   82,  114,   85,  117,   69,  101,   83,  115,   97,  111,  117,  102,
+	  110,  114,  101,   95,   98,  120,   48,   55,   95,   48,   55,   95,
+	   48,   49,   44,   95,   48,   57,   65,   70,   97,  102,   44,   58,
+	   95,   48,   57,   58,   48,   57,   58,    0
 	};
 }
 
@@ -48,9 +54,10 @@ private static final char _snakeyaml_trans_keys[] = init__snakeyaml_trans_keys_0
 private static byte[] init__snakeyaml_single_lengths_0()
 {
 	return new byte [] {
-	    0,   10,    2,    1,    1,    1,    1,    1,    1,    2,    4,    1,
-	    1,    2,    1,    1,    2,    1,    1,    1,    1,    2,    1,    1,
-	    0
+	    0,   17,    1,    1,    2,    0,    1,    2,    1,    1,    1,    1,
+	    1,    1,    4,    1,    1,    1,    1,    4,    1,    1,    2,    1,
+	    1,    2,    1,    1,    1,    2,    2,    1,    1,    0,    3,    1,
+	    1,    2,    3,    1,    1,    0,    0,    0
 	};
 }
 
@@ -60,36 +67,43 @@ private static final byte _snakeyaml_single_lengths[] = init__snakeyaml_single_l
 private static byte[] init__snakeyaml_range_lengths_0()
 {
 	return new byte [] {
+	    0,    1,    1,    1,    3,    2,    0,    0,    0,    0,    0,    0,
 	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-	    0
+	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    1,    1,
+	    1,    3,    1,    1,    0,    0,    0,    0
 	};
 }
 
 private static final byte _snakeyaml_range_lengths[] = init__snakeyaml_range_lengths_0();
 
 
-private static byte[] init__snakeyaml_index_offsets_0()
+private static short[] init__snakeyaml_index_offsets_0()
 {
-	return new byte [] {
-	    0,    0,   11,   14,   16,   18,   20,   22,   24,   26,   29,   34,
-	   36,   38,   41,   43,   45,   48,   50,   52,   54,   56,   59,   61,
-	   63
+	return new short [] {
+	    0,    0,   19,   22,   25,   31,   34,   36,   39,   41,   43,   45,
+	   47,   49,   51,   56,   58,   60,   62,   64,   69,   71,   73,   76,
+	   78,   80,   83,   85,   87,   89,   92,   95,   97,   99,  100,  105,
+	  108,  111,  117,  122,  125,  127,  128,  129
 	};
 }
 
-private static final byte _snakeyaml_index_offsets[] = init__snakeyaml_index_offsets_0();
+private static final short _snakeyaml_index_offsets[] = init__snakeyaml_index_offsets_0();
 
 
 private static byte[] init__snakeyaml_indicies_0()
 {
 	return new byte [] {
-	    0,    2,    3,    4,    5,    6,    7,    8,    9,   10,    1,   11,
-	   12,    1,   13,    1,   14,    1,   15,    1,   16,    1,   17,    1,
-	   15,    1,   15,   15,    1,   18,   15,   19,   15,    1,   15,    1,
-	   15,    1,   20,   21,    1,   14,    1,   17,    1,   22,   23,    1,
-	   15,    1,   15,    1,   12,    1,   15,    1,   19,   15,    1,   21,
-	    1,   23,    1,    1,    0
+	    0,    2,    2,    3,    5,    6,    7,    8,    9,   10,   11,   12,
+	   13,   14,   15,   16,    0,    4,    1,    3,    4,    1,   17,   17,
+	    1,   18,   18,   18,   18,   18,    1,   19,   20,    1,   21,    1,
+	   22,   23,    1,   24,    1,   25,    1,   26,    1,   27,    1,   28,
+	    1,   26,    1,   26,   29,   26,   30,    1,   31,    1,    0,    1,
+	   32,    1,    0,    1,   33,   26,   34,   26,    1,   26,    1,   26,
+	    1,   35,   36,    1,   25,    1,   28,    1,   37,   38,    1,   26,
+	    1,   26,    1,   23,    1,   26,   30,    1,   34,   26,    1,   36,
+	    1,   38,    1,    1,   39,   40,   41,   39,    1,   39,   39,    1,
+	   17,   17,    1,   18,   18,   18,   18,   18,    1,    4,   42,    4,
+	    4,    1,   42,   20,    1,   42,    1,    1,    1,    1,    0
 	};
 }
 
@@ -99,8 +113,10 @@ private static final byte _snakeyaml_indicies[] = init__snakeyaml_indicies_0();
 private static byte[] init__snakeyaml_trans_targs_0()
 {
 	return new byte [] {
-	    2,    0,    9,   10,   13,   16,   19,   20,   21,   22,   23,    3,
-	    6,    4,    5,   24,    7,    8,   11,   12,   14,   15,   17,   18
+	   33,    0,    2,   34,   38,    6,   42,    7,   14,   19,   22,   25,
+	   28,   29,   30,   31,   32,   36,   37,   39,   40,   41,    8,   11,
+	    9,   10,   43,   12,   13,   15,   17,   16,   18,   20,   21,   23,
+	   24,   26,   27,   35,    3,    4,    5
 	};
 }
 
@@ -111,7 +127,9 @@ private static byte[] init__snakeyaml_trans_actions_0()
 {
 	return new byte [] {
 	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0
+	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+	    0,    0,    0,    0,    0,    0,    0
 	};
 }
 
@@ -123,7 +141,8 @@ private static byte[] init__snakeyaml_eof_actions_0()
 	return new byte [] {
 	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
 	    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-	    1
+	    0,    0,    0,    0,    0,    0,    0,    0,    0,    5,    9,    9,
+	    9,    9,    9,    9,    9,    3,    7,    1
 	};
 }
 
@@ -135,7 +154,7 @@ static final int snakeyaml_error = 0;
 
 static final int snakeyaml_en_main = 1;
 
-// line 20 "RagelMachine.rl"
+// line 35 "RagelMachine.rl"
 
     public String scan(String scalar) {
         if (scalar == null) {
@@ -155,12 +174,12 @@ static final int snakeyaml_en_main = 1;
             data = scalar.toCharArray();
         }
         
-// line 159 "RagelMachine.java"
+// line 178 "RagelMachine.java"
 	{
 	cs = snakeyaml_start;
 	}
 
-// line 164 "RagelMachine.java"
+// line 183 "RagelMachine.java"
 	{
 	int _klen;
 	int _trans = 0;
@@ -251,7 +270,23 @@ case 4:
 // line 9 "RagelMachine.rl"
 	{ tag = "tag:yaml.org,2002:bool"; }
 	break;
-// line 255 "RagelMachine.java"
+	case 1:
+// line 10 "RagelMachine.rl"
+	{ tag = "tag:yaml.org,2002:merge"; }
+	break;
+	case 2:
+// line 11 "RagelMachine.rl"
+	{ tag = "tag:yaml.org,2002:null"; }
+	break;
+	case 3:
+// line 12 "RagelMachine.rl"
+	{ tag = "tag:yaml.org,2002:value"; }
+	break;
+	case 4:
+// line 13 "RagelMachine.rl"
+	{ tag = "tag:yaml.org,2002:int"; }
+	break;
+// line 290 "RagelMachine.java"
 		}
 	}
 	}
@@ -260,7 +295,7 @@ case 5:
 	}
 	break; }
 	}
-// line 42 "RagelMachine.rl"
+// line 57 "RagelMachine.rl"
 
 
         return tag;
--- src/main/java/org/yaml/snakeyaml/resolver/RagelMachine.rl
+++ src/main/java/org/yaml/snakeyaml/resolver/RagelMachine.rl
@@ -7,12 +7,27 @@ public class RagelMachine {
     %%{
         machine snakeyaml;
         action bool_tag { tag = "tag:yaml.org,2002:bool"; }
+        action merge_tag { tag = "tag:yaml.org,2002:merge"; }
+        action null_tag { tag = "tag:yaml.org,2002:null"; }
+        action value_tag { tag = "tag:yaml.org,2002:value"; }
+        action int_tag { tag = "tag:yaml.org,2002:int"; }
         
         Bool = ("yes" | "Yes" | "YES" | "no" | "No" | "NO" | 
                 "true" | "True" | "TRUE" | "false" | "False" | "FALSE" | 
                 "on" | "On" | "ON" | "off" | "Off" | "OFF") %/bool_tag;
+        Merge = "<<" %/merge_tag;
+        Value = "=" %/value_tag;
+        Null  = ("~" | "null" | "Null" | "null" | "NULL" | " ") %/null_tag;
+        sign = "-" | "+";
+        digit2 = digit | "_" | ",";
+        binaryInt = "0b" [0-1_]+;
+        octalInt = "0" [0-7_]+;
+        decimalInt = "0" |
+                     [1-9]digit2* (":" [0-5]? digit)*;
+        hexaInt = "0x" [0-9a-fA-F_,]+;
+        Int = sign? (binaryInt | octalInt | decimalInt | hexaInt) %/int_tag;
         
-        Scalar = Bool;
+        Scalar = Bool | Null | Int | Merge | Value;
         main := Scalar;
     }%%
     
