126f806868013abc373334bb42ca9adce8ef1a9f
somov <somov@localhost>
2009-03-02 16:11:03 +0100

    test all bool values

--- src/test/java/org/yaml/snakeyaml/resolver/RagelMachineTest.java
+++ src/test/java/org/yaml/snakeyaml/resolver/RagelMachineTest.java
@@ -20,5 +20,22 @@ public class RagelMachineTest extends TestCase {
 
     public void testScanBoolean() {
         assertEquals("tag:yaml.org,2002:bool", machine.scan("true"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("True"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("TRUE"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("false"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("False"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("FALSE"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("on"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("ON"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("On"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("off"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("Off"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("OFF"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("on"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("ON"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("On"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("off"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("Off"));
+        assertEquals("tag:yaml.org,2002:bool", machine.scan("OFF"));
     }
 }
