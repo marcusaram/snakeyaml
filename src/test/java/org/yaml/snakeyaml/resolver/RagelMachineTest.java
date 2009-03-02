1b94071287a3f13e2facfd8422b70095d3e045c5
somov <somov@localhost>
2009-03-02 16:42:43 +0100

    add merge, null, value, int to Ragel

--- src/test/java/org/yaml/snakeyaml/resolver/RagelMachineTest.java
+++ src/test/java/org/yaml/snakeyaml/resolver/RagelMachineTest.java
@@ -38,4 +38,35 @@ public class RagelMachineTest extends TestCase {
         assertEquals("tag:yaml.org,2002:bool", machine.scan("Off"));
         assertEquals("tag:yaml.org,2002:bool", machine.scan("OFF"));
     }
+
+    public void testScanNull() {
+        assertEquals("tag:yaml.org,2002:null", machine.scan("null"));
+        assertEquals("tag:yaml.org,2002:null", machine.scan("Null"));
+        assertEquals("tag:yaml.org,2002:null", machine.scan("NULL"));
+        assertEquals("tag:yaml.org,2002:null", machine.scan("~"));
+        assertEquals("tag:yaml.org,2002:null", machine.scan(" "));
+    }
+
+    public void testScanMerge() {
+        assertEquals("tag:yaml.org,2002:merge", machine.scan("<<"));
+    }
+
+    public void testScanValue() {
+        assertEquals("tag:yaml.org,2002:value", machine.scan("="));
+    }
+
+    public void testScanInt() {
+        assertEquals("tag:yaml.org,2002:int", machine.scan("0"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("1"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("-0"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("-9"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("0b0011"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("0x12ef"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("0123"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("1_000"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("1_000_000"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("+0"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("+10"));
+        assertEquals("tag:yaml.org,2002:int", machine.scan("1__000"));
+    }
 }
