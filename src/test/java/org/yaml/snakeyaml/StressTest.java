665ec85fa715dc272ffd131ffd5523dc9def403b
somov <somov@localhost>
2009-03-03 16:22:53 +0100

    add a test for many instances

--- src/test/java/org/yaml/snakeyaml/StressTest.java
+++ src/test/java/org/yaml/snakeyaml/StressTest.java
@@ -16,20 +16,35 @@ public class StressTest extends TestCase {
         doc = Util.getLocalResource("specification/example2_27.yaml");
     }
 
-    public void testPerfomance() throws IOException {
+    public void testPerfomanceOneInstance() throws IOException {
         Loader loader = new Loader(new Constructor(Invoice.class));
         Yaml yaml = new Yaml(loader);
-        int[] range = new int[] { 500, 1000 };
-        float summ = 0;
+        int[] range = new int[] { 500, 1000, 2000 };
+        System.out.println("One instance.");
         for (int number : range) {
             long time1 = System.currentTimeMillis();
             for (int i = 0; i < number; i++) {
-                Invoice invoice = (Invoice) yaml.load(doc);
-                assertNotNull(invoice);
+                yaml.load(doc);
             }
             long time2 = System.currentTimeMillis();
             float duration = (time2 - time1) / (float) number;
-            summ += duration;
+            System.out.println("Duration for r=" + number + " was " + duration + " ms/load.");
+            assertTrue("duration=" + duration, duration < 5);
+        }
+    }
+
+    public void testPerfomanceManyInstances() throws IOException {
+        int[] range = new int[] { 500, 1000, 2000 };
+        System.out.println("Many instances.");
+        for (int number : range) {
+            long time1 = System.currentTimeMillis();
+            for (int i = 0; i < number; i++) {
+                Loader loader = new Loader(new Constructor(Invoice.class));
+                Yaml yaml = new Yaml(loader);
+                yaml.load(doc);
+            }
+            long time2 = System.currentTimeMillis();
+            float duration = (time2 - time1) / (float) number;
             System.out.println("Duration for r=" + number + " was " + duration + " ms/load.");
             assertTrue("duration=" + duration, duration < 5);
         }
