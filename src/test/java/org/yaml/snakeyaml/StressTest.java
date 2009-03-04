4905d3cf0f0d42592491649648f598ef96a18e0d
somov <somov@localhost>
2009-03-04 14:38:13 +0100

    extends stress test

--- src/test/java/org/yaml/snakeyaml/StressTest.java
+++ src/test/java/org/yaml/snakeyaml/StressTest.java
@@ -12,39 +12,47 @@ import org.yaml.snakeyaml.constructor.Constructor;
 public class StressTest extends TestCase {
     String doc;
 
-    public StressTest() throws IOException {
+    public void setUp() throws IOException {
         doc = Util.getLocalResource("specification/example2_27.yaml");
     }
 
-    public void testPerfomanceOneInstance() throws IOException {
-        Loader loader = new Loader(new Constructor(Invoice.class));
-        Yaml yaml = new Yaml(loader);
-        int[] range = new int[] { 500, 1000, 2000 };
-        System.out.println("One instance.");
+    public void testPerformance() throws IOException {
+        long time1 = System.currentTimeMillis();
+        new Yaml(new Loader(new Constructor(Invoice.class)));
+        long time2 = System.currentTimeMillis();
+        float duration = time2 - time1;
+        System.out.println("Init was " + duration + " ms.");
+
+        Yaml yaml = new Yaml(new Loader(new Constructor(Invoice.class)));
+        time1 = System.currentTimeMillis();
+        yaml.load(doc);
+        time2 = System.currentTimeMillis();
+        duration = time2 - time1;
+        System.out.println("\nSingle load was " + duration + " ms.");
+
+        yaml = new Yaml(new Loader(new Constructor(Invoice.class)));
+        int[] range = new int[] { 1000, 2000 };
+        System.out.println("\nOne instance.");
         for (int number : range) {
-            long time1 = System.currentTimeMillis();
+            time1 = System.currentTimeMillis();
             for (int i = 0; i < number; i++) {
                 yaml.load(doc);
             }
-            long time2 = System.currentTimeMillis();
-            float duration = (time2 - time1) / (float) number;
+            time2 = System.currentTimeMillis();
+            duration = (time2 - time1) / (float) number;
             System.out.println("Duration for r=" + number + " was " + duration + " ms/load.");
             assertTrue("duration=" + duration, duration < 5);
         }
-    }
 
-    public void testPerfomanceManyInstances() throws IOException {
-        int[] range = new int[] { 500, 1000, 2000 };
-        System.out.println("Many instances.");
+        System.out.println("\nMany instances.");
         for (int number : range) {
-            long time1 = System.currentTimeMillis();
+            time1 = System.currentTimeMillis();
             for (int i = 0; i < number; i++) {
-                Loader loader = new Loader(new Constructor(Invoice.class));
-                Yaml yaml = new Yaml(loader);
+                yaml = new Yaml(new Loader(new Constructor(Invoice.class)));
                 yaml.load(doc);
             }
-            long time2 = System.currentTimeMillis();
-            float duration = (time2 - time1) / (float) number;
+            time2 = System.currentTimeMillis();
+            duration = (time2 - time1) / (float) number;
             System.out.println("Duration for r=" + number + " was " + duration + " ms/load.");
             assertTrue("duration=" + duration, duration < 5);
         }
