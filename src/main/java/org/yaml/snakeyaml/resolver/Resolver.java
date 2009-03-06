ca1dc3837c5717dd301af1af96ef54ce88cde378
somov <somov@localhost>
2009-03-06 22:04:10 +0100

    Restore Regular Expressions in Resolver

--- src/main/java/org/yaml/snakeyaml/resolver/Resolver.java
+++ src/main/java/org/yaml/snakeyaml/resolver/Resolver.java
@@ -12,18 +12,41 @@ import java.util.regex.Pattern;
 import org.yaml.snakeyaml.nodes.NodeId;
 
 /**
- * @see <a href="http://pyyaml.org/wiki/PyYAML">PyYAML</a> for more information
+ * @see <a href="http://pyyaml.org/wiki/PyYAML">PyYAML< /a> for more information
  */
 public class Resolver {
     private static final String DEFAULT_SCALAR_TAG = "tag:yaml.org,2002:str";
     private static final String DEFAULT_SEQUENCE_TAG = "tag:yaml.org,2002:seq";
     private static final String DEFAULT_MAPPING_TAG = "tag:yaml.org,2002:map";
+    private static final Pattern BOOL = Pattern
+            .compile("^(?:yes|Yes|YES|no|No|NO|true|True|TRUE|false|False|FALSE|on|On|ON|off|Off|OFF)$");
+    private static final Pattern FLOAT = Pattern
+            .compile("^(?:[-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+][0-9]+)?|[-+]?(?:[0-9][0-9_]*)?\\.[0-9_]+(?:[eE][-+][0-9]+)?|[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*|[-+]?\\.(?:inf|Inf|INF)|\\.(?:nan|NaN|NAN))$");
+    private static final Pattern INT = Pattern
+            .compile("^(?:[-+]?0b[0-1_]+|[-+]?0[0-7_]+|[-+]?(?:0|[1-9][0-9_]*)|[-+]?0x[0-9a-fA-F_]+|[-+]?[1-9][0-9_]*(?::[0-5]?[0-9])+)$");
+    private static final Pattern MERGE = Pattern.compile("^(?:<<)$");
+    private static final Pattern NULL = Pattern.compile("^(?:~|null|Null|NULL| )$");
+    private static final Pattern EMPTY = Pattern.compile("^$");
+    private static final Pattern TIMESTAMP = Pattern
+            .compile("^(?:[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]|[0-9][0-9][0-9][0-9]-[0-9][0-9]?-[0-9][0-9]?(?:[Tt]|[ \t]+)[0-9][0-9]?:[0-9][0-9]:[0-9][0-9](?:\\.[0-9]*)?(?:[ \t]*(?:Z|[-+][0-9][0-9]?(?::[0-9][0-9])?))?)$");
+    private static final Pattern VALUE = Pattern.compile("^(?:=)$");
+    private static final Pattern YAML = Pattern.compile("^(?:!|&|\\*)$");
 
-    private Map<Character, List<ResolverTuple>> yamlImplicitResolvers;
-    private RagelMachine ragelScanner = new RagelMachine();
+    private Map<Character, List<ResolverTuple>> yamlImplicitResolvers = new HashMap<Character, List<ResolverTuple>>();
 
     public Resolver() {
-        yamlImplicitResolvers = new HashMap<Character, List<ResolverTuple>>();
+        addImplicitResolver("tag:yaml.org,2002:bool", BOOL, "yYnNtTfFoO");
+        addImplicitResolver("tag:yaml.org,2002:float", FLOAT, "-+0123456789.");
+        addImplicitResolver("tag:yaml.org,2002:int", INT, "-+0123456789");
+        addImplicitResolver("tag:yaml.org,2002:merge", MERGE, "<");
+        addImplicitResolver("tag:yaml.org,2002:null", NULL, "~nN\0");
+        addImplicitResolver("tag:yaml.org,2002:null", EMPTY, null);
+        addImplicitResolver("tag:yaml.org,2002:timestamp", TIMESTAMP, "0123456789");
+        addImplicitResolver("tag:yaml.org,2002:value", VALUE, "=");
+        // The following implicit resolver is only for documentation purposes.
+        // It cannot work
+        // because plain scalars cannot start with '!', '&', or '*'.
+        addImplicitResolver("tag:yaml.org,2002:yaml", YAML, "!&*");
     }
 
     public void addImplicitResolver(String tag, Pattern regexp, String first) {
@@ -39,7 +62,7 @@ public class Resolver {
             for (int i = 0, j = chrs.length; i < j; i++) {
                 Character theC = new Character(chrs[i]);
                 if (theC == 0) {
-                    // special case: for null (try to match all the scalars)
+                    // special case: for null
                     theC = null;
                 }
                 List<ResolverTuple> curr = yamlImplicitResolvers.get(theC);
@@ -52,21 +75,15 @@ public class Resolver {
         }
     }
 
-    /*
-     * Use Ragel instead of Regular Expressions as in PyYAML to improve
-     * performance
-     */
     public String resolve(NodeId kind, String value, boolean implicit) {
         if (kind == NodeId.scalar && implicit) {
-            if (value.length() > 0) {
-                List<ResolverTuple> resolvers = new LinkedList<ResolverTuple>();
-                List<ResolverTuple> specific = yamlImplicitResolvers.get(value.charAt(0));
-                if (specific != null) {
-                    resolvers.addAll(specific);
-                }
-                if (yamlImplicitResolvers.containsKey(null)) {
-                    resolvers.addAll(yamlImplicitResolvers.get(null));
-                }
+            List<ResolverTuple> resolvers = null;
+            if ("".equals(value)) {
+                resolvers = yamlImplicitResolvers.get('\0');
+            } else {
+                resolvers = yamlImplicitResolvers.get(value.charAt(0));
+            }
+            if (resolvers != null) {
                 for (ResolverTuple v : resolvers) {
                     String tag = v.getTag();
                     Pattern regexp = v.getRegexp();
@@ -75,9 +92,14 @@ public class Resolver {
                     }
                 }
             }
-            String tag = ragelScanner.scan(value);
-            if (tag != null) {
-                return tag;
+            if (yamlImplicitResolvers.containsKey(null)) {
+                for (ResolverTuple v : yamlImplicitResolvers.get(null)) {
+                    String tag = v.getTag();
+                    Pattern regexp = v.getRegexp();
+                    if (regexp.matcher(value).matches()) {
+                        return tag;
+                    }
+                }
             }
         }
         // if (!yamlPathResolvers.isEmpty()) {
