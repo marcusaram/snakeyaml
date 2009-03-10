6e2f0e258df94eac3c7be6f6c2725f77d84c4783
somov <somov@localhost>
2009-03-10 14:18:26 +0100

    add a test to prove that SafeConstructor cannot create custom Java classes

--- src/test/java/org/yaml/snakeyaml/constructor/SafeConstructorTest.java
+++ src/test/java/org/yaml/snakeyaml/constructor/SafeConstructorTest.java
@@ -5,6 +5,7 @@ package org.yaml.snakeyaml.constructor;
 
 import junit.framework.TestCase;
 
+import org.yaml.snakeyaml.Loader;
 import org.yaml.snakeyaml.Yaml;
 
 public class SafeConstructorTest extends TestCase {
@@ -15,5 +16,24 @@ public class SafeConstructorTest extends TestCase {
         assertEquals(Double.POSITIVE_INFINITY, yaml.load("+.inf"));
         assertEquals(Double.POSITIVE_INFINITY, yaml.load(".inf"));
         assertEquals(Double.NEGATIVE_INFINITY, yaml.load("-.inf"));
+    }
+
+    public void testSafeConstruct() {
+        Yaml yaml = new Yaml(new Loader(new SafeConstructor()));
+        assertEquals(3.1416, yaml.load("+3.1416"));
+    }
+
+    public void testSafeConstructJavaBean() {
+        Yaml yaml = new Yaml(new Loader(new SafeConstructor()));
+        String data = "--- !!org.yaml.snakeyaml.constructor.Person\nfirstName: Andrey\nage: 99";
+        try {
+            yaml.load(data);
+            fail("JavaBeans cannot be created by SafeConstructor.");
+        } catch (ConstructorException e) {
+            assertTrue(e
+                    .getMessage()
+                    .contains(
+                            "could not determine a constructor for the tag tag:yaml.org,2002:org.yaml.snakeyaml.constructor.Person"));
+        }
     }
 }
